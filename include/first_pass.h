7c51fbb0eaea4f48885ca70f42a01453b01d343e
daniellesud <dlsudakov@gmail.com>
2024-07-20 23:08:57 +0300

    Add operand parser to miun and object operand

--- include/first_pass.h
+++ include/first_pass.h
@@ -2,5 +2,15 @@
 #define __FIRST_PASS_H__
 #include "line_parser.h"
 
+
 int firstPass(char *file_name, SymbolTable *symbolTable, int *IC, int *DC);
+/*check is there is dot ("."). reurns 1 if yes and 0 if not. */
+int is_directive(AssemblyLine* parsedLine);
+/*parse operands by miun sigintures, change values inside Operand structure in AssemblyLine*/
+int operand_parser(AssemblyLine* parsedLine);
+/*checks type of miun for every operande*/
+int check_type(Operand *operand);
+/*count occurences of some char */
+int countOccurrences(const char *str, char ch);
+
 #endif /* __FIRST_PASS_H__ */
--- src/first_pass.c
+++ src/first_pass.c
@@ -57,3 +57,125 @@ int firstPass(char *file_name, SymbolTable *symbolTable, int *IC, int *DC)
     }
     return 0;
 }
+
+int is_directive(AssemblyLine* parsedLine)
+{
+    /* Check if the first character of the instruction is a dot, as should be in directive ('.')*/
+    return (parsedLine->instruction[0] == '.') ? 1 : 0;
+}
+
+int countOccurrences(const char *str, char ch) {
+    int count = 0;
+    while (*str) {
+        if (*str == ch) {
+            count++;
+        }
+        str++;
+    }
+    return count;
+}
+
+int check_type(Operand *operand){
+    switch (operand->value[0]) 
+    {
+        case '*':
+            operand->type = 2;
+            break;
+        case '#':
+            operand->type = 0;
+            break;
+        case 'r':
+            if (strlen(operand->value) == 2 && operand->value[1] >= '0' && operand->value[1] <= '7') {
+                operand->type = 3;
+                break;
+            }
+            else {
+                /* IMPROVE ERRORS*/
+                /*supposed to be register but not */
+                return 0;
+            }
+        default:
+            operand->type = 1; /* Default is it's label, will be checked in 2 pass */
+            break;
+    }
+    return 1;
+}
+
+int operand_parser(AssemblyLine* parsedLine){
+    /*
+    TO DO - 
+        - check if up to 2 - V
+        - check type of miun - V
+        - change miun to type - V
+        - insert value to Operand structure - V
+    */
+    char *ptr_in_line = NULL;
+    ptr_in_line = parsedLine->operands;
+    Operand *srcOperand = NULL, *destOperand = NULL;
+    int srcLen = 0, destLen = 0;
+    int operandCount = 0;
+
+    srcOperand = (Operand *)malloc(sizeof(Operand));
+    destOperand = (Operand *)malloc(sizeof(Operand));
+    srcOperand->value = NULL;
+    destOperand->value = NULL;
+    
+    if (*ptr_in_line != '\0') {
+        char *start = ptr_in_line;
+        while (*ptr_in_line != '\0' && *ptr_in_line != ',') {
+            ptr_in_line++;
+            srcLen++;
+        }
+        srcOperand->value = (char *)malloc(srcLen + 1);
+        strncpy(srcOperand->value, start, srcLen);
+        srcOperand->value[srcLen] = '\0';
+        if (!check_type(srcOperand)) {
+            /* IMPROVE ERRORS */
+            free(srcOperand->value);
+            free(destOperand);
+            free(srcOperand);
+            return 0;
+        }
+        operandCount++;
+        if (*ptr_in_line == ',') ptr_in_line++; // Skip comma
+        while (*ptr_in_line != '\0' && isspace(*ptr_in_line)) ptr_in_line++; // Skip any whitespace between operands
+    }
+    /*Parse second operand*/
+    if (*ptr_in_line != '\0') {
+        char *start = ptr_in_line;
+        while (*ptr_in_line != '\0' && *ptr_in_line != ',') {
+            ptr_in_line++;
+            destLen++;
+        }
+        destOperand->value = (char *)malloc(destLen + 1);
+        strncpy(destOperand->value, start, destLen);
+        destOperand->value[destLen] = '\0';
+        if (!check_type(destOperand)) {
+            /* IMPROVE ERRORS */
+            free(srcOperand->value);
+            free(destOperand->value);
+            free(destOperand);
+            free(srcOperand);
+            return 0;
+        }
+        operandCount++;
+        if (*ptr_in_line == ',') ptr_in_line++; /*Skip comma*/
+    }
+    /*Check for extra characters after second operand*/
+    while (*ptr_in_line != '\0') {
+        if (!isspace(*ptr_in_line)) {
+            /* IMPROVE ERRORS */
+            /* there are extra chars after the second operand */
+            if (srcOperand->value) free(srcOperand->value);
+            if (destOperand->value) free(destOperand->value);
+            free(srcOperand);
+            free(destOperand);
+            return 0;
+        }
+        ptr_in_line++;
+    }
+    /* Assign operands to parsedLine*/
+    parsedLine->srcOperand = (operandCount > 0) ? srcOperand : NULL;
+    parsedLine->destOperand = (operandCount > 1) ? destOperand : NULL;
+    return 1;
+}
