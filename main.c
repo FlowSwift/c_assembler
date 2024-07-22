0a66a35341fc76548f05387e4a4d14ee3d2b253c
daniellesud <dlsudakov@gmail.com>
2024-07-22 13:34:57 +0300

    First pass ADDS - Command line checks + operand parser

--- main.c
+++ main.c
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
     
     /* pre_process(argv[1]); */
     SymbolTable *symbolTable = NULL;
+    symbolTable = createSymbolTable(); /*create empty symbol table*/
+    BinaryTable = createBinaryTable(); /*create empty binary table*/
     int IC = 0;
     int DC = 0;
     firstPass("test_firstpass", symbolTable, &IC, &DC);
--- src/util.c
+++ src/util.c
@@ -16,3 +16,55 @@ char *add_file_extension(char *filename, char *extension)
     strcat(new_filename, extension);
     return new_filename;
 }
+
+
+int isEmptyLine(char *line)
+{
+    while (*line != '\0') {
+        if (!isspace(*line)) {
+            return 0;  /* Not empty */
+        }
+        line++; 
+    }
+    return 1;
+}
+
+int isCommentedLine(char *line)
+{
+    if (*line == ';') {
+        return 1; /* Commented line*/
+    }
+    return 0;
+}
+
+
+int count_occurrences(const char *str, char ch) {
+    int count = 0;
+    while (*str!='\0') {
+        if (*str == ch) {
+            count++;
+        }
+        str++;
+    }
+    return count;
+}
+
+char *strdup(char *src_string)
+{
+    size_t len;
+    char *duplicate_string = NULL;
+
+    if (src_string == NULL)
+    {
+        return NULL;
+    }
+
+    len = strlen(src_string) + 1;
+    duplicate_string = malloc(len);
+
+    if (duplicate != NULL)
+    {
+        memcpy(duplicate_string, src_string, len);
+    }
+    return duplicate_string;
+}
