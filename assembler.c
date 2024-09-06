63fad4f45836cea5c32a65a6b6acf917b61c2f81
FlowSwift <galbarda.dev@gmail.com>
2024-09-06 16:52:43 -0400

    add prints

--- assembler.c
+++ assembler.c
@@ -17,35 +17,35 @@ int main(int argc, char *argv[])
     while (file_counter < argc)
     {
         free_all(&macro_head, &symbolTable, &binary_table);
-        /*delete*/
-        printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"); 
-        printf("File: %s\n", argv[file_counter]);
-        printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
         IC = 0, DC = 0;
+        printf("Starting pre_process for %s:\n", argv[file_counter]);
         if (pre_process(argv[file_counter], &macro_head))
         {
             printf("Error in preprocessor for the following file: %s\nWill continue to next file if available\n", argv[file_counter]);
             file_counter++;
             continue;
         }
+        printf("Done!\n");
         symbolTable = createSymbolTable(); /*create empty symbol table*/
-        printf("FIRST PASS:\n");
+        printf("Starting first_pass for %s:\n", argv[file_counter]);
         if (first_pass(argv[file_counter], macro_head, symbolTable, &binary_table, &IC, &DC))
         {
             printf("Error in first pass for the following file: %s\nWill continue to next file if available\n", argv[file_counter]);
             file_counter++;
             continue;
         }
-        printf("SECOND PASS:\n");
-        if (second_pass(argv[file_counter] ,macro_head, symbolTable, &binary_table, &IC, &DC))
+        printf("Done!\n");
+        printf("Starting second_pass for %s:\n", argv[file_counter]);
+        if (second_pass(argv[file_counter], macro_head, symbolTable, &binary_table, &IC, &DC))
         {
             printf("Error in second pass for the following file: %s\nWill continue to next file if available\n", argv[file_counter]);
             file_counter++;
             continue;
         }
+        printf("Done!\n");
         file_counter++;
     }
-    printf("FINISHED FILES\n");
+    printf("Finished all files!\n");
     free_all(&macro_head, &symbolTable, &binary_table);
     return 0;
 }
